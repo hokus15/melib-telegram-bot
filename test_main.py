9fdc9de23ca32c4af1e673ab7802cd6ebf2cf72c
hokus15 <hokus04@gmail.com>
2020-11-07 20:17:54 +0100

    refactor: Añadidos más tests unitarios

--- test_main.py
+++ test_main.py
@@ -5,8 +5,9 @@ import flask
 import unittest
 import re
 import os
+from telegram import Bot
 from unittest.mock import patch
-
+from werkzeug.exceptions import Forbidden
 
 # Evita que falle en travis-ci ya que allí no está el fichero .env
 if os.path.exists(".env"):
@@ -18,6 +19,29 @@ app = flask.Flask(__name__)
 
 
 class MelibTestCase(unittest.TestCase):
+
+    def test_webhook_autheticate_false(self):
+        with open("test_text.json") as f:
+            with patch.dict('os.environ', {'TELEGRAM_TOKEN': '0000:yyyy',
+                                           'WEBHOOK_TOKEN': 'qwertyuiop',
+                                           'VALID_USERS': '1234567890'}):
+                with app.test_request_context('?token=poiuytrewq', method="POST", data=f):
+                    rq = flask.request
+                    try:
+                        main.webhook(rq)
+                    except Exception as ex:
+                        self.assertEqual(type(ex), Forbidden)
+
+    def test_webhook_autheticate_true(self):
+        with open("test_text.json") as f:
+            with patch.dict('os.environ', {'TELEGRAM_TOKEN': '0000:yyyy',
+                                           'WEBHOOK_TOKEN': 'qwertyuiop',
+                                           'VALID_USERS': '1234567890'}):
+                with patch.object(Bot, "send_message", return_value="test message"):
+                    with app.test_request_context('?token=qwertyuiop', method="POST", data=f):
+                        rq = flask.request
+                        rs = main.webhook(rq)
+                        self.assertEqual("ok", rs)
 
     def test_autheticate_true(self):
         with open("test_text.json") as f:
